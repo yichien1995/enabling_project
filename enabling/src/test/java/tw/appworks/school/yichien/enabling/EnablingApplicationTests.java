8cd99f71b492ea04fe6494371887354c902cae24
chien <lisa241231@gmail.com>
2024-04-12 16:31:54 +0800

    Feat: Spring Boot application initialized

---
+++ enabling/src/test/java/tw/appworks/school/yichien/enabling/EnablingApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.appworks.school.yichien.enabling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnablingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
